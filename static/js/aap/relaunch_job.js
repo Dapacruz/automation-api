95184e1986927ff2c494992cdfc9d893b60b048f
David Cruz <davidcruz72@gmail.com>
2024-04-03 09:55:01 -0700

    Update relaunch job

--- static/js/aap/relaunch_job.js
+++ static/js/aap/relaunch_job.js
@@ -1,43 +1,42 @@
 // TODO: Add logic to show required fields for selected job template
 
 function relaunch_job(templ_ids) {
-    var template_name = document.getElementById("job_template_name").value
+    var template_name = document.getElementById("template_name").value
     if (template_name === "Select Job Template") {
         alert("Please select a job template")
         return
     }
-    var job_template = Object.keys(templ_ids).find(key => templ_ids[key]["name"] === template_name)
+    var template = Object.keys(templ_ids).find(key => templ_ids[key]["name"] === template_name)
     var data = JSON.stringify({
         job_template_name: template_name,
-        job_template: job_template,
-        incident: document.getElementById("incident").value,
+        job_template: template,
+        incident: document.getElementById("snow_incident").value,
         configuration_item: document.getElementById("configuration_item").value,
     })
-    console.log(data)
 
-    // fetch("/aap/relaunch/job", {
-    //     method: "POST",
-    //     body: data,
-    //     headers: {
-    //         "Accept": "application/json",
-    //         "Content-Type": "application/json",
-    //     },
-    // }).then(res => {
-    //         if (res.status > 201) {
-    //             document.getElementById("output").innerHTML = `<span style="color:red;"><i>Status ${res.status}: ${res.statusText}</i></span>`
-    //             document.getElementById("output").style.display = ""
-    //             throw new Error(`unexpected status code ${res.status}, ${res.statusText}`)
-    //         }
-    //         return res.json()
-    //     }).then(json => {
-    //         console.log(json)
-    //         document.getElementById("status").innerHTML = `${json.results}`
-    //         document.getElementById("created").innerHTML = `${json.created}`
-    //         document.getElementById("job_id").innerHTML = `${json.job_id}`
-    //         document.getElementById("url").innerHTML = `<a href="${json.url}" target="_blank">${json.url}</a>`
-    //         document.getElementById("output").style.display = ""
-    //     }).catch(err => {
-    //         console.log(err)
-    //     })
+    fetch("/aap/relaunch/job", {
+        method: "POST",
+        body: data,
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+    }).then(res => {
+            if (res.status > 201) {
+                document.getElementById("output").innerHTML = `<span style="color:red;"><i>Status ${res.status}: ${res.statusText}</i></span>`
+                document.getElementById("output").style.display = ""
+                throw new Error(`unexpected status code ${res.status}, ${res.statusText}`)
+            }
+            return res.json()
+        }).then(json => {
+            console.log(json)
+            document.getElementById("status").innerHTML = `${json.results}`
+            document.getElementById("created").innerHTML = `${json.created}`
+            document.getElementById("job_id").innerHTML = `${json.job_id}`
+            document.getElementById("url").innerHTML = `<a href="${json.url}" target="_blank">${json.url}</a>`
+            document.getElementById("output").style.display = ""
+        }).catch(err => {
+            console.log(err)
+        })
 }
 
